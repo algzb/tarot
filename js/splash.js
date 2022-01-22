dc51e6f3a42d211a6e88c710da204affc25c135b
algzb <angelgb81@gmail.com>
2022-01-22 03:24:28 +0100

    no comment

--- js/splash.js
+++ js/splash.js
@@ -14,7 +14,7 @@ $(function() {
     drawnCards.push(rider_waite_cards[currentCardNumber]);
     const currentCard = rider_waite_cards[currentCardNumber];
     console.log(currentCard)
-    $('.card_image_here').append(`<img src="${currentCard.img}" id="${(drawnCards.length - 1)}" class="drawn_card" alt="${currentCard.name}" data-toggle="modal" data-target="#explanationModal">`);   
+    $('.card_image_here').append(`<img src="${currentCard.img}" id="${(drawnCards.length - 1)}" class="drawn_card" alt="${currentCard.name}" data-toggle="modal" data-target="#explanationModal">`);
     $('.card_title_here').text(`${currentCard.name}`)
 })
 
@@ -28,6 +28,5 @@ $(document).on("click",".drawn_card", function () {
     $('.reversed_meaning_here').text(drawnCards[clicked].meta_reversed);
     $('.upright_meaning_here').text(drawnCards[clicked].meta_upright);
     $('.meta_description_here').text(drawnCards[clicked].meta_description);
-    $('.more_info_link_here').html(`<a class="btn btn-info" href="${drawnCards[clicked].meta_more_info_link}" target="_blank">Sabermás</a>`)
+    $('.more_info_link_here').html(`<a class="btn btn-info" href="${drawnCards[clicked].meta_more_info_link}" target="_blank">Saber más</a>`)
  });
- 
